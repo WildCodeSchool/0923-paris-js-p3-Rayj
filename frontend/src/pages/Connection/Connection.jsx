4581d55ee230ec1c5b77594d0b12f5c34c8c758d
yannisyannisyannis <yannis.kheyar@gmail.com>
2023-12-19 17:19:07 +0100

    page connection fini yannis

---
+++ frontend/src/pages/Connection/Connection.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./connection.css";
+import logogouv from "../../assets/logo/logogouv.png";
+import UnityTeamRAYJ from "../../assets/logo/UnityTeamRAYJ.png";
+
+function Connection() {
+  return (
+    <>
+      <img className="logo" src={logogouv} alt="logo de la société" />
+
+      <div className="login-box">
+        <div className="user-box">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            placeholder="Votre adresse e-mail"
+          />
+        </div>
+        <div className="user-box">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            required
+            placeholder="Votre mot de passe"
+          />
+        </div>
+        <div>
+          <button className="btn-connection" type="button">
+            Connexion
+          </button>
+          <hr />
+        </div>
+      </div>
+
+      <div className="inscription-box">
+        <h2 className="creationcompte">Pas encore De Compte ?</h2>
+        <button className="inscrire" type="submit">
+          <p>
+            <u>Je souhaite m'inscrire</u>
+          </p>
+        </button>
+      </div>
+
+      <img className="ray" src={UnityTeamRAYJ} alt="logo société" />
+    </>
+  );
+}
+
+export default Connection;
