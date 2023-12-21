430d6f7ffb398b9ad10d2f2fca1d66acba123701
yannisyannisyannis <yannis.kheyar@gmail.com>
2023-12-21 12:30:25 +0100

    page inscription connexion fini

--- frontend/src/pages/Inscription/inscription.jsx
+++ frontend/src/pages/Inscription/inscription.jsx
@@ -13,62 +13,71 @@ function inscription() {
 
       <from className="formulaire-inscription">
         <div className="inscription-box">
-          <input
-            className="prenom"
-            type="text"
-            name="firstname"
-            required
-            placeholder="Prénom"
-          />
-          <hr className="ligne-btn-inscription" />
-          <input
-            className="Nom"
-            type="text"
-            name="Lastname"
-            required
-            placeholder="Nom"
-          />
-          <input
-            className="email"
-            type="email"
-            name="email"
-            id="email"
-            required
-            placeholder="Votre adresse e-mail"
-          />
-          <input
-            className="password"
-            type="password"
-            name="password"
-            id="password"
-            required
-            placeholder="Votre mot de passe"
-          />
-          <input
-            className="matricule"
-            type="number"
-            name="matricule"
-            placeholder="Votre Matricule"
-          />
-          <input
-            className="phonenumber"
-            type="tel"
-            name="phoneNumber"
-            placeholder="Votre numéro de téléphone"
-          />
+          <div className="all">
+            <input
+              className="prenom"
+              type="text"
+              name="firstname"
+              required
+              placeholder="Prénom"
+            />
 
-          <label htmlFor="start" className="embauche">
-            Date d'embauche:
-            <input className="pickerdate" type="date" name="start" id="start" />
-          </label>
+            <input
+              className="Nom"
+              type="text"
+              name="Lastname"
+              required
+              placeholder="Nom"
+            />
 
-          <textarea
-            className="introduction"
-            rows="15"
-            name="message"
-            required
-            placeholder="Expliquez vos motivation"
-          />
+            <input
+              className="email"
+              type="email"
+              name="email"
+              id="email"
+              required
+              placeholder="E-mail"
+            />
+            <input
+              className="password"
+              type="password"
+              name="password"
+              id="password"
+              required
+              placeholder="Mot de passe"
+            />
+            <input
+              className="phonenumber"
+              type="tel"
+              name="phoneNumber"
+              placeholder="numéro de téléphone"
+            />
+            <input
+              className="matricule"
+              type="number"
+              name="matricule"
+              placeholder="Votre Matricule"
+            />
+            <label htmlFor="start" className="embauche">
+              Date d'embauche:
+              <input
+                className="pickerdate"
+                type="date"
+                name="start"
+                id="start"
+                value="date"
+              />
+            </label>
+
+            <textarea
+              className="introduction"
+              rows="25"
+              cols="40"
+              name="message"
+              required
+              placeholder="Expliquez vos motivation"
+            />
+          </div>
         </div>
       </from>
 
