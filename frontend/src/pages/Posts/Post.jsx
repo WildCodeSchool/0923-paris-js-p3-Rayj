e6c91348f36e525218454898e77eff9942b1c275
yannisyannisyannis <yannis.kheyar@gmail.com>
2024-01-03 16:54:07 +0100

    savaugarde de mon avance

--- frontend/src/pages/Posts/Post.jsx
+++ frontend/src/pages/Posts/Post.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+// import Select from "react-select";
+// import { useState } from "react";
 import Header from "../../components/header/Header";
 import "./post.css";
 
+// const softSkillsOptions = [
+//   { value: "Humour", label: "Humour" },
+//   { value: "Autonomie", label: "Autonomie" },
+//   { value: "Réactivité", label: "Réactivité" },
+//   { value: "Adaptabilité", label: "Adaptabilité" },
+//   { value: "Flexibilité", label: "Flexibilité" },
+// ];
+// const hardSkillsOptions = [
+//   { value: "HTML", label: "HTML" },
+//   { value: "CSS", label: "CSS" },
+//   { value: "JavaScript", label: "JavaScript" },
+//   { value: "NodeJS", label: "NodeJS" },
+//   { value: "ReactJS", label: "ReactJS" },
+// ];
+
+// const [softSkills, setSoftSkills] = useState([]);
+// const handleSelectSoftSkillChange = (selectedSoftSkillOptions) => {
+//   setSoftSkills(selectedSoftSkillOptions);
+// };
+// const [hardSkills, sethardSkills] = useState([]);
+// const handleSelectHardSkillChange = (selectedHardSkillOptions) => {
+//   sethardSkills(selectedHardSkillOptions);
+// };
 function Post() {
   return (
     <div className="pageprofiljesaispas">
@@ -51,18 +75,58 @@ function Post() {
           </label>
         </fieldset>
 
-        <label htmlFor="descriptionoffers">
-          {" "}
-          Annonce{" "}
-          <textarea
-            className="offersdescription"
-            rows="10"
-            cols="20"
-            name="message"
-            required
-            placeholder="Détaillé l'annonce"
-          />
-        </label>
+        <fieldset className="annoncedecrite">
+          <legend>Annonce</legend>
+          <label htmlFor="descriptionoffers">
+            <textarea
+              className="offersdescription"
+              rows="10"
+              cols="35"
+              name="message"
+              required
+              placeholder="Détailler l'annonce"
+            />
+          </label>
+        </fieldset>
+        <h1 className="comptetout">Compétences et Outils</h1>
+        {/* <div>
+        <Select
+          className="skill_options"
+          options={softSkillsOptions}
+          value={softSkills}
+          onChange={handleSelectSoftSkillChange}
+          isMulti
+          placeholder="Sélectionnez vos compétences..."
+        />
+        <p className="skill_block_heading">Hard Skills</p>
+        <Select
+          className="skill_options"
+          options={hardSkillsOptions}
+          value={hardSkills}
+          onChange={handleSelectHardSkillChange}
+          isMulti
+          placeholder="Sélectionnez vos compétences..."
+        />
+        </div> */}
+
+        <h2> Formation</h2>
+        <div>
+          <input type="checkbox" id="oui" name="oui" />
+          <label htmlFor="oui">oui</label>
+
+          <input type="checkbox" id="non" name="non" />
+          <label htmlFor="non">Non</label>
+        </div>
+
+        <h2>Ancienneté demandé</h2>
+        <div className="anciennetechoix">
+          <input type="checkbox" id="6mois" name="6mois" />
+          <label htmlFor="6mois">6mois</label>
+          <input type="checkbox" id="1ans" name="1ans" />
+          <label htmlFor="1ans">1ans</label>
+          <input type="checkbox" id="2ans" name="2ans" />
+          <label htmlFor="2ans">2ans</label>
+        </div>
       </div>
     </div>
   );
