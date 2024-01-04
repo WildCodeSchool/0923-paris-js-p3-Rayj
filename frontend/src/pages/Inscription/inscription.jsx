20cb5ca3054f3710b11f583842e9ca12023e251b
yannisyannisyannis <yannis.kheyar@gmail.com>
2024-01-04 17:03:57 +0100

    new futures yannis prepa au back

--- frontend/src/pages/Inscription/inscription.jsx
+++ frontend/src/pages/Inscription/inscription.jsx
@@ -1,26 +1,56 @@
-import React from "react";
-
+import { useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import logogouv from "../../assets/logo/logogouv.png";
 import UnityTeamRAYJ from "../../assets/logo/UnityTeamRAYJ.png";
 import "./inscription.css";
 
 function inscription() {
+  const [firstName, setFirstname] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setphoneNumber] = useState("");
+  const [seniority, setSeniority] = useState("");
+  const [pickerdate, setPickerdate] = useState("");
+  const [introduction, setIntroduction] = useState("");
+
+  const handlesubmit = useCallback(
+    async (event) => {
+      event.preventDefault();
+
+      const response = await fetch("api/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ firstName, lastName }),
+      });
+      if (response.ok) {
+        console.info("Form submitted successfully");
+      } else {
+        console.error("Failed to submit the form");
+      }
+    },
+    [firstName, lastName]
+  );
+
   const validation = useNavigate();
 
   return (
     <div className="inscribody">
       <img className="logorf" src={logogouv} alt="logo de la société" />
 
-      <from className="form-inscription">
+      <form onSubmit={handlesubmit} className="form-inscription">
         <div className="inscription-box">
           <div className="all">
             <input
               className="firstname"
               type="text"
-              name="firstname"
+              name="firstName"
               required
+              value={firstName}
               placeholder="Prénom"
+              onChange={(event) => setFirstname(event.target.value)}
             />
 
             <input
@@ -29,6 +59,7 @@ function inscription() {
               name="Lastname"
               required
               placeholder="Nom"
+              onChange={(event) => setLastName(event.target.value)}
             />
 
             <input
@@ -36,8 +67,10 @@ function inscription() {
               type="email"
               name="email"
               id="email"
+              value={email}
               required
               placeholder="E-mail"
+              onChange={(event) => setEmail(event.target.value)}
             />
             <input
               className="password"
@@ -45,19 +78,25 @@ function inscription() {
               name="password"
               id="password"
               required
+              value={password}
               placeholder="Mot de passe"
+              onChange={(event) => setPassword(event.target.value)}
             />
             <input
               className="phonenumber"
               type="tel"
               name="phoneNumber"
               placeholder="numéro de téléphone"
+              value={phoneNumber}
+              onChange={(event) => setphoneNumber(event.target.value)}
             />
             <input
               className="seniority"
               type="number"
               name="matricule"
               placeholder="Votre Matricule"
+              value={seniority}
+              onChange={(event) => setSeniority(event.target.value)}
             />
             <label htmlFor="start" className="embauche">
               Date d'embauche:
@@ -66,7 +105,8 @@ function inscription() {
                 type="date"
                 name="start"
                 id="start"
-                value="date"
+                value={pickerdate}
+                onChange={(event) => setPickerdate(event.target.value)}
               />
             </label>
 
@@ -77,19 +117,21 @@ function inscription() {
               name="message"
               required
               placeholder="Expliquez vos motivation"
+              value={introduction}
+              onChange={(event) => setIntroduction(event.target.value)}
             />
           </div>
         </div>
-      </from>
-      <div>
-        <button
-          className="validation"
-          type="button"
-          onClick={() => validation("/profil")}
-        >
-          Valider
-        </button>
-      </div>
+        <div>
+          <button
+            className="validation"
+            type="submit"
+            onClick={() => validation("/profil")}
+          >
+            Valider
+          </button>
+        </div>
+      </form>
       <div>
         <img className="rayinscrir" src={UnityTeamRAYJ} alt="logo société" />
       </div>
@@ -98,3 +140,5 @@ function inscription() {
 }
 
 export default inscription;
+
+// voir authentification gerer l'auto-login v2//
