0e1b326f4f069fb72298c0a28dfa8a01618a4049
Asle Sayed <asle@DESKTOP-8JMLA5A.localdomain>
2024-02-05 11:56:05 +0100

    fixing css

--- frontend/src/components/header/ProfilHeader.jsx
+++ frontend/src/components/header/ProfilHeader.jsx
@@ -1,14 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import rara from "../../assets/images/rara.png";
 import Arrow from "../../assets/images/return_arrow.svg";
 import "./profilHeader.css";
 
 function ProfilHeader() {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div className="profil_img_name">
       <img className="photoprofil" src={rara} alt="logo de profil" />
       <h2 className="profilname">Rachid Taha</h2>
       <div className="lesbuttons">
-        <button type="button" className="button-precedent">
+        <button type="button" className="button-precedent" onClick={goBack}>
           <img className="flecheprec" alt="" src={Arrow} />
         </button>
         <button type="button" className="butbutprofil">
